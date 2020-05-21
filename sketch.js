af540ba40e8480105c18da22c5d9e44b8aa73d85
itsmelpalermo <65146646+itsmelpalermo@users.noreply.github.com>
2020-05-21 18:26:27 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -59,7 +59,7 @@ function setup() {
 	imageMode(CENTER);
 	image(backgroundImage, 0, 0);
 
-	dateLine = 'Updated: May 19, 2020';
+	dateLine = 'Updated: May 20, 2020';
 	textAlign(RIGHT);
 	noStroke();
 	fill(241, 239, 227);
