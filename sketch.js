d71f189568202d6183b93c675e1ac001b9343210
itsmelpalermo <65146646+itsmelpalermo@users.noreply.github.com>
2020-05-28 18:53:18 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -59,7 +59,7 @@ function setup() {
 	imageMode(CENTER);
 	image(backgroundImage, 0, 0);
 
-	dateLine = 'Updated: May 26, 2020';
+	dateLine = 'Updated: May 27, 2020';
 	textAlign(RIGHT);
 	noStroke();
 	fill(241, 239, 227);
@@ -249,7 +249,7 @@ function setup() {
 
 		sourceTxt.position(1024, 512);
 
-		//save('Date_LTCDeaths.png');
+		save('Date_LTCDeaths.png');
 
 }
 
